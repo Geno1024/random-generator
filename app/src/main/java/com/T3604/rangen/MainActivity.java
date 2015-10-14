ce9be375d44d6d6280bb9f8f797ba13322a3c4a1
Geno1024 <754097987@qq.com>
2015-10-14 19:08:46 +0800

    [update] What am I doing?

--- app/src/main/java/com/T3604/rangen/MainActivity.java
+++ app/src/main/java/com/T3604/rangen/MainActivity.java
@@ -1,59 +1,56 @@
 package com.T3604.rangen;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.preference.PreferenceManager;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends Activity
 {
+	public SharedPreferences sp;
+	public SharedPreferences.Editor e;
+	public LinearLayout list;
+	public EditText in;
+	public Button add;
+	public View.OnLongClickListener l;
+	public int count;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
-
-		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-		fab.setOnClickListener(new View.OnClickListener()
+		sp = PreferenceManager.getDefaultSharedPreferences(this);
+		e = sp.edit();
+		list = (LinearLayout) findViewById(R.id.mainList);
+		in = (EditText) findViewById(R.id.text);
+		add = (Button) findViewById(R.id.btn);
+		l = new View.OnLongClickListener()
 		{
 			@Override
-			public void onClick(View view)
+			public boolean onLongClick(View v)
 			{
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
+				list.removeView(v);
+				return false;
 			}
-		});
-	}
+		};
+		
+		count = sp.getInt("count", 0);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item)
-	{
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings)
+		if (count == 0) list.removeView(findViewById(R.id.desc));
+		else
 		{
-			return true;
+			for (int i = 0; i < count; i++)
+			{
+				TextView t = new TextView(this);
+				t.setText(sp.getString("_" + i, ""));
+				t.setOnLongClickListener(l);
+			}
 		}
-
-		return super.onOptionsItemSelected(item);
 	}
 }
