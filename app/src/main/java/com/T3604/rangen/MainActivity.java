6895049c8c260f0c8156bc1d95fc3b3dcc2c716f
Geno1024 <754097987@qq.com>
2015-10-14 20:17:04 +0800

    {RELEASE}

--- app/src/main/java/com/T3604/rangen/MainActivity.java
+++ app/src/main/java/com/T3604/rangen/MainActivity.java
@@ -3,16 +3,25 @@ package com.T3604.rangen;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.io.File;
+import java.util.Locale;
 import java.util.Random;
 
 public class MainActivity extends Activity
@@ -124,5 +133,60 @@ public class MainActivity extends Activity
 		for (int i = 0; i < count; i++)
 			e.putString("_" + i, ((TextView) list.getChildAt(i)).getText().toString());
 		e.commit();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		menu.add(Menu.NONE, Menu.NONE, 1, R.string.about);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		String inst = getString(R.string.desc);
+		PackageManager pm = getPackageManager();
+		LinearLayout layout = new LinearLayout(this);
+		ImageView appImage = new ImageView(this);
+		Drawable appDrawable = null;
+		TextView appName = new TextView(this);
+		TextView version = new TextView(this);
+		String vName = null;
+		int vCode = 0;
+		int apkSize = 0;
+		try
+		{
+			appDrawable = pm.getApplicationIcon(pm.getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA));
+			appName.setText(pm.getApplicationLabel(pm.getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA)));
+			vName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+			vCode = getPackageManager().getPackageInfo(getPackageName(), 0).versionCode;
+			apkSize = (int)new File(getPackageManager().getApplicationInfo(getPackageName(), 0).sourceDir).length();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		appImage.setImageDrawable(appDrawable);
+		appName.setTypeface(Typeface.MONOSPACE);
+		if(Locale.getDefault().equals(Locale.CHINESE) || Locale.getDefault().equals(Locale.CHINA))
+			version.setText("\nBy Geno.\nY. Z. Chen\n电子邮箱：754097987@qq.com\n\n版本号：" + vCode + "\n版本名称：" + vName + "\n安装包大小：" + apkSize + "\n\n" + inst);
+		else
+			version.setText("\nBy Geno.\nY. Z. Chen\nEmail:754097987@qq.com\n\nVersion Code:" + vCode + "\nVersion Name:" + vName + "\nPackageSize:" + apkSize + "\n\n" + inst);
+		version.setTypeface(Typeface.MONOSPACE);
+		version.setGravity(Gravity.CENTER);
+
+		layout.setOrientation(LinearLayout.VERTICAL);
+		layout.setGravity(Gravity.CENTER);
+		layout.addView(appImage, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+		layout.addView(appName, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+		layout.addView(version, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+		AlertDialog.Builder ad = new AlertDialog.Builder(this)
+				.setTitle(R.string.about)
+				.setView(layout)
+				.setPositiveButton(android.R.string.ok, null);
+		ad.show();
+		return super.onOptionsItemSelected(item);
 	}
 }
